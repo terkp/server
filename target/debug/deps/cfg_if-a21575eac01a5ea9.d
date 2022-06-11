87dc23450efc19b972844657a4547c32ba82778c
Skadic <etieti22@gmail.com>
2022-06-11 17:26:10 +0200

    gitignore

--- target/debug/deps/cfg_if-a21575eac01a5ea9.d
+++
@@ -1,7 +0,0 @@
-D:\Programmieren\rust\server\server\server\target\debug\deps\cfg_if-a21575eac01a5ea9.rmeta: C:\Users\init3\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-D:\Programmieren\rust\server\server\server\target\debug\deps\libcfg_if-a21575eac01a5ea9.rlib: C:\Users\init3\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-D:\Programmieren\rust\server\server\server\target\debug\deps\cfg_if-a21575eac01a5ea9.d: C:\Users\init3\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\init3\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
--- target/debug/server.d
+++
@@ -1,1 +0,0 @@
-D:\Programmieren\rust\server\server\server\target\debug\server.exe: D:\Programmieren\rust\server\server\server\src\main.rs
