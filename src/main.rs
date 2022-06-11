e8a513f65a0fb7bb33336b746ff9941de5eaa17f
Skadic <etieti22@gmail.com>
2022-06-11 18:48:28 +0200

    Rocket Base

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-use std::io;
-use rand::Rng;
+#[macro_use] extern crate rocket;
 
-fn main() {
-    println!("Guess the number!");
+#[get("/hello")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number);
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
