acee143c270db92aeb585d4ac54737cd00bfd16f
Skadic <etieti22@gmail.com>
2023-05-26 02:06:59 +0200

    make display remember whether to show solutions and answers

--- frontend/display/index.js
+++ frontend/display/index.js
@@ -5,6 +5,32 @@ editCSS.innerHTML = ".answerText {display: none;}";
 document.body.appendChild(editCSS);
 updateGroups();
 
+{
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/questions/state", true);
+  xhr.onload = (e) => {
+    console.log(xhr.status + ": " + xhr.responseText);
+    if (xhr.status === 200) {
+      let groupQuestionState = JSON.parse(xhr.responseText);
+      localStorage.setItem(
+        "answerIsShown",
+        groupQuestionState.questionState.answerIsShown
+      );
+      localStorage.setItem(
+        "solutionIsShown",
+        groupQuestionState.questionState.solutionIsShown
+      );
+      if (groupQuestionState.questionState.solutionIsShown) {
+        document.getElementById("solution").style.display = "block";
+      }
+      if (groupQuestionState.questionState.answerIsShown) {
+        editCSS.innerHTML += ".groupAnswerText { display: block; }";
+      }
+    }
+  };
+  xhr.send(localStorage.getItem("group_name"));
+}
+
 if (typeof EventSource !== "undefined") {
   //create an object, passing it the name and location of the server side script
   var eSource = new EventSource("/events");
@@ -13,8 +39,10 @@ if (typeof EventSource !== "undefined") {
     //write the received data to the page
     if (event.data === "show_solution") {
       document.getElementById("solution").style.display = "block";
+      localStorage.setItem("solutionIsShown", true);
     } else if (event.data === "show_answers") {
       editCSS.innerHTML = ".groupAnswerText {display: block;}";
+      localStorage.setItem("answerIsShown", true);
     } else if (event.data === "toggle_leaderboard") {
       window.location.href = "/display/leaderboard";
     } else if (event.data === "groups") {
@@ -49,8 +77,7 @@ function updateGroups() {
   xhr.onload = (e) => {
     answersElement.innerHTML = "";
     let answers = JSON.parse(xhr.responseText);
-    for (var groupName in answers) {
-      let answer = answers[groupName];
+    for (const [groupName, answer] of Object.entries(answers).sort()) {
       if (groupName.length > 20) {
         groupName = groupName.substring(0, 18) + "...";
       }
